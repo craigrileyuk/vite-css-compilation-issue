bb5f05a22b6d3aa16d96e9e760f8a92af2f7110d
C. Riley <hello@craigriley.uk>
2021-06-24 20:51:35 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <ColorPicker />
+</template>
+
+<script setup>
+import ColorPicker from "primevue/colorpicker";
+</script>
+
+<style>
+@import "primevue/resources/themes/saga-blue/theme.css";
+@import "primevue/resources/primevue.min.css";
+@import "primeicons/primeicons.css";
+@import "primevue/resources/themes/mdc-dark-deeppurple/theme.css";
+</style>
